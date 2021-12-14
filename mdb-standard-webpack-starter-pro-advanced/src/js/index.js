d3487336d226ef29d14ef4a67f7edb278a5a6870
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2021-12-13 22:30:02 -0700

    added jumnbotron to header section and applied background image

---
+++ mdb-standard-webpack-starter-pro-advanced/src/js/index.js
@@ -0,0 +1,85 @@
+import * as mdb from 'mdb-ui-kit';
+
+import Calendar from 'mdb-calendar';
+import Captcha from 'mdb-captcha';
+import ColorPicker from 'mdb-color-picker';
+import Cookie from 'mdb-cookies';
+import Countdown from 'mdb-countdown';
+import DataParser from 'mdb-data-parser';
+import { Draggable, Sortable } from 'mdb-drag-and-drop';
+import Dummy from 'mdb-dummy';
+import EcommerceGallery from 'mdb-ecommerce-gallery';
+import FileUpload from 'mdb-file-upload';
+import Filters from 'mdb-filters';
+import InputMask from 'mdb-input-mask';
+import Mention from 'mdb-mention';
+import MultiCarousel from 'mdb-multi-carousel';
+import Onboarding from 'mdb-onboarding';
+import OrganizationChart from 'mdb-organization-chart';
+import Parallax from 'mdb-parallax';
+import ScrollStatus from 'mdb-scroll-status';
+import Storage from 'mdb-storage';
+import TableEditor from 'mdb-table-editor';
+import Transfer from 'mdb-transfer';
+import Treetable from 'mdb-treetable';
+import Treeview from 'mdb-treeview';
+import VectorMap from 'mdb-vector-maps';
+import WYSIWYG from 'mdb-wysiwyg-editor';
+
+window.mdb = mdb;
+window.Calendar = Calendar;
+window.Captcha = Captcha;
+window.ColorPicker = ColorPicker;
+window.Cookie = Cookie;
+window.Countdown = Countdown;
+window.DataParser = DataParser;
+window.Draggable = Draggable;
+window.Sortable = Sortable;
+window.Dummy = Dummy;
+window.EcommerceGallery = EcommerceGallery;
+window.FileUpload = FileUpload;
+window.Filters = Filters;
+window.InputMask = InputMask;
+window.Mention = Mention;
+window.MultiCarousel = MultiCarousel;
+window.Onboarding = Onboarding;
+window.OrganizationChart = OrganizationChart;
+window.Parallax = Parallax;
+window.ScrollStatus = ScrollStatus;
+window.StoragePlugin = Storage;
+window.TableEditor = TableEditor;
+window.Transfer = Transfer;
+window.Treetable = Treetable;
+window.Treeview = Treeview;
+window.VectorMap = VectorMap;
+window.WYSIWYG = WYSIWYG;
+
+export default {
+  mdb,
+  Calendar,
+  Captcha,
+  ColorPicker,
+  Cookie,
+  Countdown,
+  DataParser,
+  Draggable,
+  Sortable,
+  Dummy,
+  EcommerceGallery,
+  FileUpload,
+  Filters,
+  InputMask,
+  Mention,
+  MultiCarousel,
+  Onboarding,
+  OrganizationChart,
+  Parallax,
+  ScrollStatus,
+  Storage,
+  TableEditor,
+  Transfer,
+  Treetable,
+  Treeview,
+  VectorMap,
+  WYSIWYG,
+};
