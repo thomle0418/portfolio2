b79e5e9298eaafa7ff2541f2378986792c32e714
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2021-12-17 01:10:27 -0700

    starting over due to corrupt files...

--- mdb-standard-webpack-starter-pro-advanced/getMDBsrc.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-
-try {
-  fs.copySync('node_modules/mdb-ui-kit/src', 'mdb')
-  console.log('MDB UI KIT source code successfully copied!');
-} catch (err) {
-  console.log('Failed to copy MDB UI KIT source code!');
-}
--- python-free-simple/index.py
+++
@@ -1,6 +0,0 @@
-from functools import partial
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-handler = partial(SimpleHTTPRequestHandler, directory='./public')
-server_object = HTTPServer(('', 3000), handler)
-server_object.serve_forever()
