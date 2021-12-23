15df60177b30d1ab6c111b11854eab26a52a5ecb
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2021-12-22 22:05:34 -0700

    added js on.click function to unhide about me section

---
+++ script.js
@@ -0,0 +1,16 @@
+const aboutMe= document.querySelector(".aboutMeContent");
+const learnMore= document.querySelector(".btn")
+
+displayAboutMe=()=>{
+aboutMe.classList.remove("d-none");
+}
+
+learnMore.addEventListener("click", displayAboutMe);
+
+// function startQuiz(){
+//     //remove display none from info card
+//     infoCard.classList.remove("d-none");
+//     }
+//     //when start button is clicked, display info card
+//     startButton.addEventListener("click",startQuiz);
+    
