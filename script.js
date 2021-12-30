45b82740b6da595eb44798c981d37a436bf9075f
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2021-12-30 13:36:44 -0700

    added JS function to menu links to reveal respective sections & added new font family

--- script.js
+++ script.js
@@ -17,10 +17,24 @@ displayProjectCard=()=>{
 }
 projectButton.addEventListener("click",displayProjectCard);
 
+const aboutMeMenu= document.querySelector(".aboutMeMenu");
+const projectsMenu= document.querySelector(".projectsMenu");
+const contactmeMenu= document.querySelector(".contactMeMenu");
 
+displayAboutMeMenu=()=>{
+    aboutMe.classList.remove("d-none");
+}
+aboutMeMenu.addEventListener("click",displayAboutMeMenu);
 
+displayProjectMenu=()=>{
+    projectCard.classList.remove("d-none");
+}
+projectsMenu.addEventListener("click",displayProjectMenu);   
 
-
+displayContactMenu=()=>{
+    projectCard.classList.remove("d-none");
+}
+contactmeMenu.addEventListener("click",displayContactMenu);
 // function startQuiz(){
 //     //remove display none from info card
 //     infoCard.classList.remove("d-none");
